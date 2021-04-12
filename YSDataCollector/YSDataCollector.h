6782b5a4cd126c50aa11013d8198307ab5e8b903
Alim Osipov <yellstr@gmail.com>
2021-04-12 12:49:53 +0300

    template

---
+++ YSDataCollector/YSDataCollector.h
@@ -0,0 +1,18 @@
+//
+//  YSDataCollector.h
+//  YSDataCollector
+//
+//  Created by Alim Osipov on 12.04.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YSDataCollector.
+FOUNDATION_EXPORT double YSDataCollectorVersionNumber;
+
+//! Project version string for YSDataCollector.
+FOUNDATION_EXPORT const unsigned char YSDataCollectorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YSDataCollector/PublicHeader.h>
+
+
